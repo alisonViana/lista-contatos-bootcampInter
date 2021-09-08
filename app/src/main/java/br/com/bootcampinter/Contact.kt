894130a628e310fc5929f6edb31c7f20edff8d28
alisonViana <alisonviana.c@gmail.com>
2021-09-08 20:22:16 -0300

    created contact class and updated contact adapter

---
+++ app/src/main/java/br/com/bootcampinter/Contact.kt
@@ -0,0 +1,10 @@
+package br.com.bootcampinter
+
+/**
+ * Classe que representa cada contato da lista
+ */
+data class Contact(
+    val name: String,
+    val phone: String,
+    val photograph: String
+)
--- app/src/main/java/br/com/bootcampinter/ContactAdapter.kt
+++ app/src/main/java/br/com/bootcampinter/ContactAdapter.kt
@@ -15,17 +15,28 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
         TODO("Not yet implemented")
     }
 
+    /**
+     * Método responsável por percorrer cada item do array, obter o valor e preencher na tela
+     *
+     * ou seja, ler o item no array e popular no RecycleView
+     */
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
     }
 
+    /**
+     * Requisito da classe Adapter: quantos itens tem na lista
+     */
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
     /**
      * Classe responsável pelo gerenciamento de cada item
      */
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(contact: Contact) {
+
+        }
     }
 }
