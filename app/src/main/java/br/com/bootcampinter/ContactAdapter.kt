2320ba97c574232935dcc7181fa1ce9128321117
alisonViana <alisonviana.c@gmail.com>
2021-09-08 19:37:30 -0300

    created git repository

---
+++ app/src/main/java/br/com/bootcampinter/ContactAdapter.kt
@@ -0,0 +1,14 @@
+package br.com.bootcampinter
+
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Classe responsável por gerenciar a lista de contatos como um todo
+ */
+class ContactAdapter: RecyclerView.Adapter<>() {
+
+    /**
+     * Classe responsável pelo gerenciamento de cada item
+     */
+    class ContactAdapterViewHolder:
+}
