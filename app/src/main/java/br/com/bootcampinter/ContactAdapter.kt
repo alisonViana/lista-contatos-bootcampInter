5714c927526c7b6eaeffc8d61e6e8cbd8389316b
alisonViana <alisonviana.c@gmail.com>
2021-09-08 20:04:31 -0300

    created contact adapter class

--- app/src/main/java/br/com/bootcampinter/ContactAdapter.kt
+++ app/src/main/java/br/com/bootcampinter/ContactAdapter.kt
@@ -1,14 +1,31 @@
 package br.com.bootcampinter
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Classe responsável por gerenciar a lista de contatos como um todo
  */
-class ContactAdapter: RecyclerView.Adapter<>() {
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
+
+    private val list: MutableList<Contact> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 
     /**
      * Classe responsável pelo gerenciamento de cada item
      */
-    class ContactAdapterViewHolder:
+    class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
 }
