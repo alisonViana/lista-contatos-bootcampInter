899c84969bd8b52134126cce2936512f04ed4983
alisonViana <alisonviana.c@gmail.com>
2021-09-09 00:41:00 -0300

    updated main activity

--- app/src/main/java/br/com/bootcampinter/MainActivity.kt
+++ app/src/main/java/br/com/bootcampinter/MainActivity.kt
@@ -2,10 +2,35 @@ package br.com.bootcampinter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        val contact1 = Contact("Alison Viana", "(11)00000-0000", "img.png")
+        val contact2 = Contact("Maria Luiza", "(12) 11111-1111", "img.png")
+
+        adapter.updateList(
+            arrayListOf(contact1, contact2)
+        )
     }
 }
