cce99809a23f5d44906d731d1a69e0abf4252071
alisonViana <alisonviana.c@gmail.com>
2021-09-10 20:02:46 -0300

    created options menu

--- app/src/main/java/br/com/bootcampinter/MainActivity.kt
+++ app/src/main/java/br/com/bootcampinter/MainActivity.kt
@@ -2,10 +2,18 @@ package br.com.bootcampinter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * variável que recebe a RecyclerView
+     */
     private val rvList: RecyclerView by lazy {
         findViewById(R.id.rv_list)
     }
@@ -32,5 +40,32 @@ class MainActivity : AppCompatActivity() {
         adapter.updateList(
             arrayListOf(contact1, contact2)
         )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * Função responsável pela criação do Options Menu
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_meu_1 -> {
+                showToast("Selecionado o item 1")
+                return true
+            }
+            R.id.item_meu_2 -> {
+                showToast("Selecionado o item 2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
