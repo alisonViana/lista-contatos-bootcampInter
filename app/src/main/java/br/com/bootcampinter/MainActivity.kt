dc079d1635d72eefb6a8adc5cc16f4048a83e151
alisonViana <alisonviana.c@gmail.com>
2021-09-12 20:43:38 -0300

    created drawer menu

--- app/src/main/java/br/com/bootcampinter/MainActivity.kt
+++ app/src/main/java/br/com/bootcampinter/MainActivity.kt
@@ -2,13 +2,17 @@ package br.com.bootcampinter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     /**
@@ -22,10 +26,47 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
+        setNavegationViewListener()
         bindViews()
         updateList()
+    }
+
+    private fun setNavegationViewListener() {
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        navView.setNavigationItemSelectedListener{ item ->
+            when(item.itemId){
+                R.id.item_menu_1 -> {
+                    showToast("MENU 1")
+                    true
+                }
+                R.id.item_menu_2 -> {
+                    showToast("MENU 2")
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
     }
 
     private fun bindViews() {
@@ -57,11 +98,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.item_meu_1 -> {
+            R.id.item_menu_1 -> {
                 showToast("Selecionado o item 1")
                 return true
             }
-            R.id.item_meu_2 -> {
+            R.id.item_menu_2 -> {
                 showToast("Selecionado o item 2")
                 return true
             }
