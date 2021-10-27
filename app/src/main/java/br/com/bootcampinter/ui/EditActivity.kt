c7cb0c2a90872350703aa406dc8e342cc9e4cd76
alisonViana <alisonviana.c@gmail.com>
2021-10-27 19:21:51 -0300

    implements auto formatting phone number

--- app/src/main/java/br/com/bootcampinter/ui/EditActivity.kt
+++ app/src/main/java/br/com/bootcampinter/ui/EditActivity.kt
@@ -2,34 +2,34 @@ package br.com.bootcampinter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.widget.Toolbar
-import androidx.lifecycle.ViewModelProvider
 import br.com.bootcampinter.data.model.Contact
 import br.com.bootcampinter.R
+import br.com.bootcampinter.databinding.ContactEditBinding
 import br.com.bootcampinter.presentation.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class EditActivity() : AppCompatActivity() {
-/*
+
     private var contact: Contact? = null
     private var newContactFlag: Boolean = false
-    private val mainViewModel: MainViewModel =
-        ViewModelProvider.NewInstanceFactory().create(MainViewModel::class.java)
+    private val viewModel by viewModel<MainViewModel>()
+    private val binding by lazy { ContactEditBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.contact_edit)
+        setContentView(binding.root)
 
         initToolbar()
-        setOnClickListeners()
+        setListeners()
         getExtras()
     }
 
     private fun initToolbar() {
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
@@ -39,7 +39,7 @@ class EditActivity() : AppCompatActivity() {
      */
     private fun getExtras() {
         if (intent.hasCategory(NEW_CONTACT)) {
-            setViewNewContact()
+            binding.btnDeleteContact.visibility = View.GONE
             newContactFlag = true
         }
         else{
@@ -49,23 +49,13 @@ class EditActivity() : AppCompatActivity() {
         }
     }
 
-    private fun setViewNewContact() {
-        val btnDeleteContact = findViewById<Button>(R.id.btn_delete_contact)
-
-        btnDeleteContact.visibility = View.GONE
-    }
-
     /**
      * Seta as informações do contato nas views correspondentes
      */
     private fun bindView() {
-        val etName = findViewById<TextView>(R.id.et_name)
-        val etPhone = findViewById<TextView>(R.id.et_phone)
-        val ivPhoto = findViewById<ImageView>(R.id.iv_photograph)
-
-        etName.text = contact?.name
-        etPhone.text = contact?.phone
-        ivPhoto.setImageResource(contact!!.photograph)
+        binding.newContactCard.etName.setText(contact?.name)
+        binding.newContactCard.etPhone.setText(contact?.phone)
+        binding.ivPhotograph.setImageResource(contact!!.photograph)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -76,33 +66,34 @@ class EditActivity() : AppCompatActivity() {
     /**
      * Cria o listener e configra as ações do botão salvar
      */
-    private fun setOnClickListeners() {
-        val btnSaveContact = findViewById<Button>(R.id.btn_save_contact)
-        val btnDeleteContact = findViewById<Button>(R.id.btn_delete_contact)
-        val etName = findViewById<EditText>(R.id.et_name)
-        val etPhone = findViewById<EditText>(R.id.et_phone)
+    private fun setListeners() {
+        binding.newContactCard.etPhone.addTextChangedListener(PhoneNumberFormattingTextWatcher("BR"))
 
         // Listener do botão salvar
-        btnSaveContact.setOnClickListener {
-            if (etName.text.toString().isNullOrBlank() or etPhone.text.toString().isNullOrBlank()) {
+        binding.btnSaveContact.setOnClickListener {
+            if (binding.newContactCard.etName.text.toString().isBlank() or binding.newContactCard.etPhone.text.toString().isBlank()) {
                 showToast("Verifique os dados do contato!")
             } else {
                 if (newContactFlag) {
-                    // TODO val newContact = Contact(etName.text.toString(), etPhone.text.toString())
-                    // TODO mainViewModel.addContact(newContact)
+                    val newContact = Contact(
+                        name = binding.newContactCard.etName.text.toString(),
+                        phone = binding.newContactCard.etPhone.text.toString())
+                    viewModel.addContact(newContact)
                 }
                 else {
-                    // TODO val editedContact = Contact(etName.text.toString(), etPhone.text.toString(), contact?.id)
-                    // TODO mainViewModel.editContact(editedContact)
+                    val editedContact = Contact(
+                        id = contact!!.id,
+                        name = binding.newContactCard.etName.text.toString(),
+                        phone = binding.newContactCard.etPhone.text.toString())
+                    viewModel.editContact(editedContact)
                 }
-
                 showToast("Contato salvo!")
                 finish()
             }
         }
 
         // Listener do botão deletar
-        btnDeleteContact.setOnClickListener {
+        binding.btnDeleteContact.setOnClickListener {
             showAlertDialogDeleteContact()
         }
     }
@@ -117,7 +108,7 @@ class EditActivity() : AppCompatActivity() {
 
         builder.apply {
             setPositiveButton(R.string.ad_positive){ _, _ ->
-                // TODO mainViewModel.deleteContact(contact?.id)
+                viewModel.deleteContact(contact!!)
                 showToast("Contato Excluido!")
                 finish()
             }
@@ -137,5 +128,4 @@ class EditActivity() : AppCompatActivity() {
         const val NEW_CONTACT: String = "NEW_CONTACT"
     }
 
- */
 }
